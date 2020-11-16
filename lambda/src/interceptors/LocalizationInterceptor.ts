52486c638ffdb22c9e8c1abf0ffcdb991a0ef167
Nathan Friend <nathan@gitlab.com>
2020-11-15 19:05:34 -0500

    Fixed eslint

---
+++ lambda/src/interceptors/LocalizationInterceptor.ts
@@ -0,0 +1,52 @@
+import * as Alexa from 'ask-sdk-core';
+import * as fs from 'fs';
+import i18n from 'i18next';
+import * as path from 'path';
+import * as util from 'util';
+
+export class LocalizationInterceptor implements Alexa.RequestInterceptor {
+  private translationsPromise: Promise<{ [lang: string]: any }>;
+
+  constructor() {
+    const readdirAsync = util.promisify(fs.readdir);
+
+    const I18N_DIR = path.resolve(__dirname, '../../i18n');
+
+    /** A promise that returns an object containing all available translations */
+    this.translationsPromise = (async () => {
+      const langs = (
+        await readdirAsync(I18N_DIR, {
+          withFileTypes: true,
+        })
+      )
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name);
+
+      const translations: { [lang: string]: any } = {};
+
+      langs.forEach((l) => {
+        translations[l] = {
+          translation: require(path.resolve(I18N_DIR, l, 'translation.json')),
+        };
+      });
+
+      return translations;
+    })();
+  }
+
+  async process(handlerInput: Alexa.HandlerInput) {
+    const lng = handlerInput.requestEnvelope.request.locale;
+
+    await i18n.init({
+      lng,
+      fallbackLng: 'en',
+      resources: await this.translationsPromise,
+      returnEmptyString: false,
+      keySeparator: false,
+      nsSeparator: false,
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  }
+}
