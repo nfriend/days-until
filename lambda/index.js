f4fecd001de5a43ac9c7e81f13c0d9fc998e7cd5
Nathan Friend <nathan@gitlab.com>
2020-11-26 23:11:41 -0500

    Make node understand ~

--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,7 @@
+// Allow node to understand the "~" path alias
+// See https://www.npmjs.com/package/module-alias
+require('module-alias/register');
+
 // Necessary because Lambda doesn't respect
 // package.json's "main" property. (It only
 // executes a root "index.js".)
