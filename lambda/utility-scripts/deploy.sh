7b7677cc455eb88940ddc4acb73f2e8e159b28a1
Nathan Friend <nathan@gitlab.com>
2020-11-26 23:48:27 -0500

    Commit changes back to GitLab, if any

--- lambda/utility-scripts/deploy.sh
+++ lambda/utility-scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-pushd $CI_PROJECT_DIR/lambda
+pushd "$CI_PROJECT_DIR/lambda" || exit
 
 # Prune node_modules down to only those necessary for production
-yarn install --prod --frozen-lockfile --cache-folder $CI_PROJECT_DIR/.yarn
+yarn install --prod --frozen-lockfile --cache-folder "$CI_PROJECT_DIR/.yarn"
 
 # Print the size of node_modules
-echo node_modules size: $(du -hs node_modules)
+echo node_modules size: "$(du -hs node_modules)"
 
-pushd $CI_PROJECT_DIR
+pushd "$CI_PROJECT_DIR" || exit
 
 # Install envsubst and sponge
 apt-get update
@@ -19,29 +19,32 @@ envsubst < ask-resources.json | sponge ask-resources.json
 cat ask-resources.json
 
 # Add ask-cli globally and deploy
-yarn global add ask-cli --cache-folder $CI_PROJECT_DIR/.yarn
+yarn global add ask-cli --cache-folder "$CI_PROJECT_DIR/.yarn"
 ask deploy
 
 # Commit any changes made to infrastructure/cfn-deployer/skill-stack.yaml
 # and push the result back to the default branch
-if [[ -z $(git status -s) ]]; then
+if [[ -n $(git status -s) ]]; then
   echo "Found the following changes in infrastructure/cfn-deployer/skill-stack.yaml:"
   git status
 
   git config user.name "days-until-bot"
-  git config user.name "days-until-bot@nathanfriend.io"
-  git remote set-url --push origin $CI_REPOSITORY_URL
+  git config user.email "days-until-bot@nathanfriend.io"
+
+  REPO_URL="${CI_SERVER_PROTOCOL}://${COMMITTER_USER}:${COMMITTER_TOKEN}@${CI_SERVER_URL#http*//}/${CI_PROJECT_PATH}.git"
+  echo "Pushing to ${REPO_URL}..."
+  git remote set-url --push origin "${REPO_URL}"
 
   git add infrastructure/cfn-deployer/skill-stack.yaml
-  git commit -m 'Update skill-stack.yaml from deployment'
-  git push -o ci.skip
+  git commit -m 'Update some stuff automatically'
+  git push origin "HEAD:${CI_DEFAULT_BRANCH}" -o ci.skip
 else
   echo "No changes detected to infrastructure/cfn-deployer/skill-stack.yaml"
 fi
 
-popd
+popd || exit
 
 # Run a regular yarn install to restore node_modules before it's cached
-yarn install --frozen-lockfile --cache-folder $CI_PROJECT_DIR/.yarn
+yarn install --frozen-lockfile --cache-folder "$CI_PROJECT_DIR/.yarn"
 
-popd
+popd || exit
