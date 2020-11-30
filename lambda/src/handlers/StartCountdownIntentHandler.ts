ff201484b58655d30da2e48108995f89fe55a89a
Nathan Friend <nathan@gitlab.com>
2020-11-29 21:33:31 -0500

    Remove non-background sound effects

--- lambda/src/handlers/StartCountdownIntentHandler.ts
+++ lambda/src/handlers/StartCountdownIntentHandler.ts
@@ -43,46 +43,6 @@ export class StartCountdownIntentHandler implements Alexa.RequestHandler {
 
     speeches.push(
       chooseOne(
-        // Typing Short (2)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/office/amzn_sfx_typing_short_02"/>',
-        ),
-
-        // Swoosh Fast 1x (1)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/foley/amzn_sfx_swoosh_fast_1x_01"/>',
-        ),
-
-        // Hits (10)
-        i18n.t('<audio src="soundbank://soundlibrary/wood/hits/hits_10"/>'),
-
-        // Movement (8)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/telephones/movement/movement_08"/>',
-        ),
-
-        // Screens (10)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/computers/screens/screens_10"/>',
-        ),
-
-        // Watches (6)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/clocks/watches/watches_06"/>',
-        ),
-
-        // Hits (6)
-        i18n.t('<audio src="soundbank://soundlibrary/glass/hits/hits_06"/>'),
-
-        // Glasses Clink (2)
-        i18n.t(
-          '<audio src="soundbank://soundlibrary/foley/amzn_sfx_glasses_clink_02"/>',
-        ),
-      ),
-    );
-
-    speeches.push(
-      chooseOne(
         i18n.t('Done!'),
         i18n.t("You're all set!"),
         i18n.t('Got it!'),
