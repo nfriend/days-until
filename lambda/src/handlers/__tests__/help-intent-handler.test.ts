9fd34b2518a193b6cf551556b963e9274604734a
Nathan Friend <nathan@gitlab.com>
2020-12-10 16:07:24 -0500

    Add help note about the list all feature

--- lambda/src/handlers/__tests__/help-intent-handler.test.ts
+++ lambda/src/handlers/__tests__/help-intent-handler.test.ts
@@ -29,9 +29,10 @@ describe('helpIntentHandler', () => {
 
     expect(result).toSpeek(
       [
-        'Start by creating a new countdown. You can do this by saying "Ask Days Until to create a new countdown".',
-        'Once you\'ve created a countdown, you can check on its status by saying "how many days until my birthday", for example.',
-        'You can delete a countdown by saying "Ask Days Until to delete a countdown".',
+        'Start by creating a new countdown. You can do this by saying, "Ask Days Until to create a new countdown".',
+        'Once you\'ve created a countdown, you can check on its status by saying, "how many days until my birthday", for example.',
+        'Or, check all your countdowns by saying, "list all my countdowns".',
+        'You can delete a countdown by saying, "Ask Days Until to delete a countdown".',
         "There's also a few other things I can do. See this skill's description in the Alexa App for a complete list.",
         'Hopefully that helped! Now, what would you like to do?',
       ].join(' '),
--- lambda/src/handlers/help-intent-handler.ts
+++ lambda/src/handlers/help-intent-handler.ts
@@ -20,13 +20,16 @@ export const helpIntentHandler: Alexa.RequestHandler = {
 
     const speak = [
       i18n.t(
-        'Start by creating a new countdown. You can do this by saying "Ask Days Until to create a new countdown".',
+        'Start by creating a new countdown. You can do this by saying, "Ask Days Until to create a new countdown".',
       ),
       i18n.t(
-        'Once you\'ve created a countdown, you can check on its status by saying "how many days until my birthday", for example.',
+        'Once you\'ve created a countdown, you can check on its status by saying, "how many days until my birthday", for example.',
       ),
       i18n.t(
-        'You can delete a countdown by saying "Ask Days Until to delete a countdown".',
+        'Or, check all your countdowns by saying, "list all my countdowns".',
+      ),
+      i18n.t(
+        'You can delete a countdown by saying, "Ask Days Until to delete a countdown".',
       ),
       i18n.t(
         "There's also a few other things I can do. See this skill's description in the Alexa App for a complete list.",
